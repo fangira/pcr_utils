0bf250a13c5c5cd464f6e2e78d4dc4fc4e8ff2be
ted <763957208@qq.com>
2020-07-19 23:16:18 +0800

    1

--- pcr/src/views/Home.vue
+++ pcr/src/views/Home.vue
@@ -3,19 +3,71 @@
     <a-form id="form1">
       <a-row :gutter="24">
         <a-col :span="3">
-          <a-form-item label="当前值：">
-            <a-input-number v-model="currentLevel" :min="1" :max="maxLevel" @change="onChange" />
+          <h2>等级</h2>
+        </a-col>
+        <a-col :span="4">
+          <a-form-item label="需要经验：">
+            <div>{{offsetExp}}</div>
           </a-form-item>
         </a-col>
-        <a-col :span="3">
-          <a-form-item label="目标值：">
-            <a-input-number
-              v-model="targetLevel"
-              :min="currentLevel"
-              :max="maxLevel"
-              @change="onChange"
-            />
+      </a-row>
+      <a-row :gutter="24">
+        <a-col :span="4">
+          <a-form-item label="当前等级：">
+            <a-input-number v-model="currentLevel" :min="1" :max="maxLevel" />
           </a-form-item>
+        </a-col>
+        <a-col :span="4">
+          <a-form-item label="当前经验值：">
+            <a-input-number v-model="currentExp" :min="0" :max="9999" />
+          </a-form-item>
+        </a-col>
+        <a-col :span="4">
+          <a-form-item label="目标等级：">
+            <a-input-number v-model="targetLevel" :min="currentLevel" :max="maxLevel" />
+          </a-form-item>
+        </a-col>
+      </a-row>
+      <a-row :gutter="24">
+        <a-col :span="3">
+          <h2>每日获取经验</h2>
+        </a-col>
+        <a-col :span="4">
+          <a-form-item label="每日获得总经验：">
+            <div>{{expPerDay}}</div>
+          </a-form-item>
+        </a-col>
+      </a-row>
+      <a-row :gutter="24">
+        <a-col :span="4">
+          <a-form-item label="日回复体力：">
+            <a-input-number v-model="natureTP" :min="0" />
+          </a-form-item>
+        </a-col>
+        <a-col :span="7">
+          <a-form-item label="每日任务（350经验+200体力）">
+            <a-input-number v-model="missionExp" :min="0" />
+          </a-form-item>
+        </a-col>
+      </a-row>
+      <a-row :gutter="24">
+        <a-col :span="5">
+          <a-form-item label="家具体力日产量">{{homeTP}}</a-form-item>
+        </a-col>
+        <a-col :span="5">
+          <a-form-item label="通关地下城数量(N*48)">
+            <a-input-number v-model="homeToolsNumber" :min="0" />
+          </a-form-item>
+        </a-col>
+        <a-col :span="5">
+          <a-form-item label="N管体力(N*120)">
+            <a-input-number v-model="numberTP" :min="0" :max="30" />
+          </a-form-item>
+        </a-col>
+      </a-row>
+      <a-row :gutter="24">
+        <a-col :span="6">
+          <a-form-item label="所需">{{needDays}}天</a-form-item>
         </a-col>
       </a-row>
       <a-row :gutter="24">
@@ -37,9 +89,6 @@
           <a-form-item label="Field B">
             <a-input placeholder="input placeholder" />
           </a-form-item>
-        </a-col>
-        <a-col :span="6">
-          <div>答案：{{result}}</div>
         </a-col>
       </a-row>
     </a-form>
@@ -64,16 +113,21 @@ export default {
       currentLevel: 90,
       // 目标等级
       targetLevel: 98,
-      // 计算结果
-      result: 0
+      // 当前经验值
+      currentExp: 0,
+      // 自然回体
+      natureTP: 240,
+      // 每日任务获取经验(TODO:按照有没有买mana和有没有会战,和双倍)
+      missionExp: 550,
+      // 通关地下城数量
+      homeToolsNumber: 3,
+      // 多少管体力
+      numberTP: 0
     };
   },
-  methods: {
-    onChange(value) {
-      console.log("changed", value);
-      this.calExp();
-    },
-    calExp() {
+  computed: {
+    // 还差多少经验
+    offsetExp() {
       let currentExp;
       let targetExp;
       levelMap.forEach(item => {
@@ -84,14 +138,26 @@ export default {
           targetExp = item.totalNeed;
         }
       });
-      // 经验差
-      let offsetExp = targetExp - currentExp;
-      console.log("offsetExp", offsetExp);
-      this.result = offsetExp;
+      return targetExp - currentExp - this.currentExp;
+    },
+    // 家具体力
+    homeTP() {
+      return 176 + 48 * this.homeToolsNumber;
+    },
+    // 每日经验总量
+    expPerDay() {
+      return (
+        this.natureTP + this.missionExp + this.homeTP + this.numberTP * 120
+      );
+    },
+    // 所需天数
+    needDays() {
+      console.log(this.offsetExp / this.expPerDay);
+      if (this.offsetExp / this.expPerDay <= 0) {
+        return 0;
+      }
+      return Math.ceil(this.offsetExp / this.expPerDay);
     }
-  },
-  created() {
-    this.calExp();
   }
 };
 </script>
@@ -101,5 +167,8 @@ export default {
 }
 #form1 .ant-form-item-control-wrapper {
   flex: 1;
+}
+h2 {
+  text-align: left;
 }
 </style>
